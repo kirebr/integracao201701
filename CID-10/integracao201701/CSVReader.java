f9b75fe022390005d314e866dae2951497a72d24
Erivan <erivan.nas@gmail.com>
2017-04-03 20:03:51 -0300

    Primeiro Commit
    Criada estrutura de projeto java e incluídos arquivos da CID-10.CSV e
    algoritmo que pesquisa a palavra chave dengue no arquivo e imprime as
    linhas que contenham a palavra.

---
+++ CID-10/integracao201701/CSVReader.java
@@ -0,0 +1,34 @@
+package integracao201701;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVReader 
+{
+	public static void main(String[] args ) throws IOException
+	{
+	
+		String keyWord = "dengue";
+		
+		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\alunoinf\\Documents\\GitHub\\integracao201701\\CID-10\\CID-10-SUBCATEGORIAS.CSV"));
+		
+		
+		String line = "";
+		
+		while ((line = br.readLine()) != null) {
+			
+			if( line.contains( keyWord))
+			{
+				System.out.println(line);
+			}
+		    // "," ou ";" de acordo com o arquivo
+		   // String[] row = line.split(";");
+	
+		   // System.out.println(row[0] + " - " + row[1]);
+			
+			//System.out.println( line);
+		}
+	
+	}
+}
